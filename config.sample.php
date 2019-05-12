e98b001296567e2ffcbc4a8cbfc710641b07fbf9
James Phillips <james@jp2.me.uk>
2019-05-12 21:35:27 +0100

    Remove mailman config from config sample

--- config.sample.php
+++ config.sample.php
@@ -2,8 +2,4 @@
 // Whether or not to enable twig caching
 // This makes template generation faster but changes might not be seen straight away
 $twigCaching = true;
-
-// Mailman Configuration
-$listsHost = 'https://your.list.server';
-$listsPwd = 'yourListPassword';
 ?>
